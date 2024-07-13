bde035c14ac6c087305fbbb6ad9b41a4f4946d98
Al. Ja. M.-U. <150540760+aljamu@users.noreply.github.com>
2024-07-13 21:29:54 +0200

    Basic Routing (ohne Express)
    Routen wie http://localhost:3000/home ,
    http://localhost:3000/contacts ,
    http://localhost:3000/404 -> Page not found , etc.
    
    ohne Express

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ console.log("Listening to port 3000")
 */
 
 /*SERVE JSON:
----------------------------------------*/
+---------------------------------------
 var http = require("http");
 var fs = require("fs");
 
@@ -172,6 +172,33 @@ var server = http.createServer(function(req, res){
         alter: 45
     }
     res.end(JSON.stringify(myObj)) //Dateien zurück an Client senden
+})
+
+server.listen(3000, '127.0.0.1')
+console.log("Listening to port 3000")
+*/
+
+/*BASIC ROUTING:
+---------------------------------------*/
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function(req, res){
+    console.log(`Request was made: ${req.url}`)
+    if(req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }else if(req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    }else if(req.url === '/api/ninjas'){
+        var ninjas = [{name: "James", age: 29}, {name: "Yoshi", age: 22}]
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(ninjas))
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
 })
 
 server.listen(3000, '127.0.0.1')

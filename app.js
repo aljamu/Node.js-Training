b6e1b6c1b07416d5f0f05268067da42b90ac2f03
Al. Ja. M.-U. <150540760+aljamu@users.noreply.github.com>
2024-07-13 18:03:17 +0200

    Globales Objekt
    Methoden ausprobieren

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+console.log("Hello World");
+console.log("Du befindest dich in (dir): " + __dirname) //Node kann anzeigen, in welcher dir oder welcher Datei wir sind
+console.log("Du befindest dich in (file): " + __filename) //Node kann anzeigen, in welcher dir oder welcher Datei wir sind
+
+
+setTimeout(function(){
+    console.log("3 setTimeout-seconds have passed")
+}, 3000); // nach 3 Sekunden output:"3 seconds have passed"
+
+/* var time = 0;
+setInterval(function(){
+    time += 2;
+    console.log(time + " seconds have passed")
+}, 2000); // alle 2 Sekunden Output: 2/4/6/8/etc. seconds have passed 
+PROBLEM: Der Code läuft unendlich lange weiter. 
+Besser: */
+
+var time = 0;
+var timer = setInterval(function(){
+    time += 1;
+    console.log(time + " setInterval-seconds have passed")
+    if (time > 2){
+        clearInterval(timer);
+    }
+}, 2000);
+
+
